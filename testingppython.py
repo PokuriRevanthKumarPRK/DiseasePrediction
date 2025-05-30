8df880c41f7c11c7845a950350dc8738a3f16d2c
PokuriRevanthKumarPRK <pokuri_revanth_kumar@students.edu.sg>
2025-05-31 00:53:15 +0800

    Update testingppython.py

--- testingppython.py
+++ testingppython.py
@@ -12,7 +12,7 @@ def load_model():
 
 @st.cache_resource
 def load_generator():
-    return pipeline("text2text-generation", model="google/flan-t5-base")
+    return pipeline("text2text-generation", model="google/bio-ul2")
 
 model = load_model()
 generator = load_generator()
